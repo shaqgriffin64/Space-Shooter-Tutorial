de0665c69638218721ced2860dc2e1355482be8b
shaqgriffin64 <55068788+shaqgriffin64@users.noreply.github.com>
2019-11-10 23:10:13 -0500

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private AudioSource audioSource;
+
+    private Rigidbody rb;
+    private float nextFire;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+                audioSource.Play();
+        }   
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+             0.0f,
+             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        //controls the player's tilt in game, set as a public value
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
---
+++ Scripts/gameController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text score;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int scoreValue;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        scoreValue = 0;
+        UpdateScore ();
+        StartCoroutine (SpawnWaves());
+        //score.text = "Score: " + scoreValue.ToString();
+    }
+
+    private void Update()
+    {
+        if (restart)
+            {
+                if (Input.GetKeyDown (KeyCode.R))
+                    {
+                        SceneManager.LoadScene("SampleScene");
+                    }
+            }
+
+        if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
+    }
+
+    IEnumerator SpawnWaves ()
+        {
+            yield return new WaitForSeconds(startWait);
+            while (true)
+            {
+                for (int i = 0; i < hazardCount; i++)
+                    {
+                        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                        Quaternion spawnRotation = Quaternion.identity;
+                        Instantiate(hazard, spawnPosition, spawnRotation);
+                        yield return new WaitForSeconds(spawnWait);
+
+                        if (gameOver)
+                            {
+                                restartText.text = "Press 'R' for Restart";
+                                restart = true;
+                                break;
+                            }
+                    }
+                yield return new WaitForSeconds(waveWait);
+            }
+        }
+
+    public void AddScore (int newScoreValue)
+        {
+            scoreValue += newScoreValue;
+            UpdateScore();
+        }
+
+    void UpdateScore ()
+        {
+        score.text = "Score: " + scoreValue.ToString();
+        }
+
+    public void GameOver ()
+        {
+            gameOverText.text = "Game Over! Game made by Shaquille Griffin";
+            gameOver = true;
+        }
+}
